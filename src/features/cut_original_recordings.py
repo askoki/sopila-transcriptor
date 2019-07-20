2dd28a112abb9b5bee33a08f48ee1ccd4ddf87b2
Arian <23128190+askoki160@users.noreply.github.com>
2019-07-20 21:27:13 +0200

    Adapte cutting to all models

--- src/features/cut_original_recordings.py
+++ src/features/cut_original_recordings.py
@@ -2,6 +2,7 @@ import os
 import sys
 sys.path.insert(1, os.path.join(sys.path[0], '..', '..'))
 
+from settings import CUT_DIR
 from helpers.file_helpers import create_directory, clear_dir
 from pydub import AudioSegment
 from settings import NUMBER_OF_CORES
@@ -13,48 +14,55 @@ if not len(sys.argv) > 1:
 else:
     step = int(sys.argv[1])
 
-# alternative dir
-if len(sys.argv) > 2:
-    from settings import REAL_DATA_RAW as RAW_DATA_DIR, REAL_DATA_CUT as CUT_DIR
-else:
-    from settings import RAW_DATA_DIR, CUT_DIR
+if not sys.argv[2]:
+    print('Enter model name defined in settings.py')
+    sys.exit()
+
+ML_MODEL = str(sys.argv[2])
+
+if not sys.argv[3]:
+    print('Enter path of raw data dir')
+    sys.exit()
+
+RAW_DATA_DIR = str(sys.argv[3])
 
 
 def cut_folder_files(folder):
-    folder_files = listdir(os.path.join(RAW_DATA_DIR, folder))
-    create_directory(os.path.join(CUT_DIR, folder))
+    folder_files = listdir(os.path.join(RAW_DATA_DIR, ML_MODEL, folder))
+    create_directory(os.path.join(CUT_DIR, ML_MODEL, folder))
 
     numeration = 0
     for file in folder_files:
         start = 0
         # in miliseconds
         title = str(step) + "ms"
-        audioFile = AudioSegment.from_wav(
-            os.path.join(RAW_DATA_DIR, folder, file)
+        audio_file = AudioSegment.from_wav(
+            os.path.join(RAW_DATA_DIR, ML_MODEL, folder, file)
         )
 
         # measured in miliseconds
-        duration = len(audioFile)
+        duration = len(audio_file)
         number_of_segments = int(duration / step)
 
         for i in range(0, number_of_segments):
             end = start + step
-            newAudio = audioFile[start:end]
+            new_audio = audio_file[start:end]
 
-            filepath = os.path.join(
-                CUT_DIR, folder, title + str(numeration) + '.wav'
+            file_path = os.path.join(
+                CUT_DIR, ML_MODEL, folder, title + str(numeration) + '.wav'
             )
-            newAudio.export(filepath, format="wav")
+            new_audio.export(file_path, format="wav")
             start += step
             numeration += 1
 
 # -------- PARALLELIZE ----------
 from multiprocessing import Pool
 
-if __name__ == '__main__':
-    clear_dir(os.path.join(CUT_DIR))
 
-    recordings_folders = listdir(os.path.join(RAW_DATA_DIR))
+if __name__ == '__main__':
+    clear_dir(os.path.join(CUT_DIR, ML_MODEL))
+
+    recordings_folders = listdir(os.path.join(RAW_DATA_DIR, ML_MODEL))
     recordings_folders.sort()
     with Pool(processes=NUMBER_OF_CORES) as pool:
         pool.map(cut_folder_files, recordings_folders)
