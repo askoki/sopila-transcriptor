e3190b0c712cf5aafda9803590887e003907a21a
Arian <23128190+askoki160@users.noreply.github.com>
2019-07-21 13:26:26 +0200

    Add functions for rf and cnn statistics

--- src/features/helpers/data_helpers.py
+++ src/features/helpers/data_helpers.py
@@ -9,8 +9,7 @@ mpl.use('TkAgg')
 import matplotlib.pyplot as plt
 
 sys.path.insert(0, os.path.join(sys.path[0], '..', '..'))
-from settings import FIGURES_DIR, PROCESSED_DATA_DIR, \
-    CUT_DIR, STATISTICS_DIR
+from settings import FIGURES_DIR, PROCESSED_DATA_DIR, STATISTICS_DIR
 
 
 def natural_sort(l):
@@ -28,15 +27,58 @@ def calculate_f1(precision, recall):
     return (2 * precision * recall) / (precision + recall)
 
 
-def write_model_statistics(title, history_dict, input_shape, validation_split, parameters):
+def write_rf_model_statistics(title, metrics_dict, training_shape,
+                              test_shape, parameters, model_name):
     """
     title -> string reporesenting name of the model
+    metrics_dict -> dict containing accuracy, precision and recall metrics
+    training_shape, test_shape -> tuple containing training and test data size
+    parameters -> dict containing tested parameters in and iteration
+    model_name -> name of the folder where statistics will be saved 
+    (defined according to ML_MODELS in settings.py)
+    """
+    with open(os.path.join(STATISTICS_DIR, model_name, title + '.txt'), 'w+') as f:
+        print('n_estimators ' + str(parameters[0]), file=f)
+        print('criterion ' + str(parameters[1]), file=f)
+        print('bootstrap ' + str(parameters[2]), file=f)
+        print('min_samples_split ' + str(parameters[3]), file=f)
+        print('min_samples_leaf ' + str(parameters[4]), file=f)
+        print('max_features ' + str(parameters[5]), file=f)
+        print('max_depth ' + str(parameters[6]), file=f)
+
+        print('Training_shape ' + str(training_shape.shape), file=f)
+        print('Test_shape ' + str(test_shape.shape), file=f)
+        print('Training_accuracy ' + str(metrics_dict['acc_train']), file=f)
+        print('Training_precision ' +
+              str(metrics_dict['precision_train']), file=f)
+        print('Training_recall ' + str(metrics_dict['recall_train']), file=f)
+        print('Training_F1 ' + str(calculate_f1(
+            metrics_dict['precision_train'],
+            metrics_dict['recall_train'])), file=f
+        )
+        print('Test_accuracy ' + str(metrics_dict['acc_test']), file=f)
+        print('Test_precision ' +
+              str(metrics_dict['precision_test']), file=f)
+        print('Test_recall ' + str(metrics_dict['recall_test']), file=f)
+        print('Test_F1 ' + str(calculate_f1(
+            metrics_dict['precision_test'],
+            metrics_dict['recall_test'])), file=f
+        )
+
+
+def write_cnn_model_statistics(title, history_dict, input_shape,
+                               validation_split, parameters, model_name):
+    """
+    title -> string representing name of the model
     history_dict -> keras dictionary containing model statistics
     input_shape -> tuple containing training data size
     validation_split -> float describing training - validation split
     parameters -> dict containing tested parameters in and iteration
+    model_name -> name of the folder where statistics will be saved 
+    (defined according to ML_MODELS in settings.py)
     """
-    with open(os.path.join(STATISTICS_DIR, title + '.txt'), 'w+') as f:
+    file_path = os.path.join(STATISTICS_DIR, model_name, title + '.txt')
+    with open(file_path, 'w+') as f:
         validation_num = int(input_shape[0] * validation_split)
         training_num = int(input_shape[0] - validation_num)
         print('Cnn_layers ' + str(parameters[0]), file=f)
@@ -48,8 +90,9 @@ def write_model_statistics(title, history_dict, input_shape, validation_split, p
         print('Training_data ' + str(training_num), file=f)
         print('Validation_data ' + str(validation_num), file=f)
         print('Training_accuracy ' + str(history_dict['acc'][0]), file=f)
-        print('Training_precision ' +
-              str(history_dict['precision'][0]), file=f)
+        print(
+            'Training_precision ' + str(history_dict['precision'][0]), file=f
+        )
         print('Training_recall ' + str(history_dict['recall'][0]), file=f)
         print('Training_F1 ' + str(calculate_f1(
             history_dict['precision'][0],
@@ -66,12 +109,60 @@ def write_model_statistics(title, history_dict, input_shape, validation_split, p
         )
 
 
-def plot_model_statistics(title, train, valid, data_name):
+def plot_rf_model_statistics(title, train_score, val_score, data_name, model_name):
+    '''
+    title -> string representing figure title
+    train_score -> Float presenting train accuracy
+    val_score -> Float presenting validation accuracy
+    data_name -> string describing title
+    model_name -> name of the folder where statistics will be saved 
+    (defined according to ML_MODELS in settings.py)
+    '''
+    # summarize history for accuracy
+    plt.figure(title.capitalize())
+    plt.bar([0, 1], [train_score, val_score])
+    plt.xticks([0, 1], ('train', 'validation'))
+    plt.title('model %s' % (title.lower()))
+    plt.ylim(0, 1)
+
+    train_label = "%.4g%%" % (train_score * 100)
+    plt.text(
+        0,
+        0.5,
+        train_label,
+        fontweight='bold',
+        horizontalalignment='center',
+        verticalalignment='center'
+    )
+
+    val_label = "%.4g%%" % (val_score * 100)
+    plt.text(
+        1,
+        0.5,
+        val_label,
+        fontweight='bold',
+        horizontalalignment='center',
+        verticalalignment='center'
+    )
+
+    img_name = 'train_test_' + title.lower() + '_' + data_name + '.png'
+
+    plt.savefig(
+        os.path.join(FIGURES_DIR, model_name, img_name),
+        format='png',
+        dpi=300
+    )
+    plt.close()
+
+
+def plot_cnn_model_statistics(title, train, valid, data_name, model_name):
     """
-    title -> string reporesenting figure title
+    title -> string representing figure title
     train -> list of keras values during n epoch on the training set
     valid -> list of keras values during n epoch on the validation set
     data_name -> string describing title
+    model_name -> name of the folder where statistics will be saved 
+    (defined according to ML_MODELS in settings.py)
     """
     # summarize history for accuracy
     plt.figure(title.capitalize())
@@ -85,14 +176,21 @@ def plot_model_statistics(title, train, valid, data_name):
 
     img_name = title.lower() + '_' + data_name + '.png'
 
-    plt.savefig(os.path.join(FIGURES_DIR, img_name), format='png', dpi=300)
+    plt.savefig(
+        os.path.join(FIGURES_DIR, model_name, img_name),
+        format='png',
+        dpi=300
+    )
     plt.close()
 
 
-def plot_confusion_matrix(cm, classes, matrix_name, normalize=False, title='', cmap=plt.cm.Blues):
+def plot_confusion_matrix(cm, classes, matrix_name, model_name, normalize=False, title='', cmap=plt.cm.Blues):
     """
     This function prints and plots the confusion matrix.
     Normalization can be applied by setting `normalize=True`.
+
+    model_name -> name of the folder where statistics will be saved 
+    (defined according to ML_MODELS in settings.py)
 
     Code is taken from scikit-learn.org example.
     """
@@ -129,7 +227,9 @@ def plot_confusion_matrix(cm, classes, matrix_name, normalize=False, title='', c
     plt.tight_layout()
 
     plt.savefig(
-        os.path.join(FIGURES_DIR, 'confusion_matrix_' + matrix_name + '.png'),
+        os.path.join(
+            FIGURES_DIR, model_name, 'confusion_matrix_' + matrix_name + '.png'
+        ),
         format='png',
         dpi=500
     )
@@ -139,7 +239,7 @@ def plot_confusion_matrix(cm, classes, matrix_name, normalize=False, title='', c
 def get_min_data_size(data_path, is_unfiltered):
     """
     data_path -> Path to the processed (but not splitted) data
-    is_unfiltered -> boolean value determining weather data 
+    is_unfiltered -> boolean value determining weather data
     was preprocessed or not
 
     Returns size of matrix with lowest size (needed for balanced dataset)
@@ -159,10 +259,10 @@ def get_min_data_size(data_path, is_unfiltered):
 def get_data(data_path, is_unfiltered):
     """
     data_path -> Path to the processed (but not splitted) data
-    is_unfiltered -> boolean value determining weather data 
+    is_unfiltered -> boolean value determining weather data
     was preprocessed or not
 
-    Returns tuple containing list of all amlitudes (X) and all_labels (y)
+    Returns tuple containing list of all amplitudes (X) and all_labels (y)
     """
     all_values = []
     all_labels = []
@@ -185,9 +285,15 @@ def get_data(data_path, is_unfiltered):
     return (all_values, all_labels)
 
 
-def get_train_data():
+def get_train_data(model_name):
+    """
+    model_name -> string representing processed folder in which
+    processed data is located.
+
+    Returns tuple containing features and labels for train data.
+    """
     file = h5py.File(os.path.join(
-        PROCESSED_DATA_DIR, 'processed_data.hdf5'), 'r')
+        PROCESSED_DATA_DIR, model_name, 'processed_data.hdf5'), 'r')
 
     return (
         file['x_train'].value,
@@ -195,9 +301,15 @@ def get_train_data():
     )
 
 
-def get_test_data():
+def get_test_data(model_name):
+    """
+    model_name -> string representing processed folder in which
+    processed data is located.
+
+    Returns tuple containing features and labels for test data.
+    """
     file = h5py.File(os.path.join(
-        PROCESSED_DATA_DIR, 'processed_data.hdf5'), 'r')
+        PROCESSED_DATA_DIR, model_name, 'processed_data.hdf5'), 'r')
 
     return (
         file['x_test'].value,
@@ -205,14 +317,20 @@ def get_test_data():
     )
 
 
-def get_folder_class_index(folder_name):
+def get_folder_class_index(cut_dir_path, folder_name):
+    """
+    cut_dir_path -> String representing path of the cut directory.
 
-    class_labels = os.listdir(CUT_DIR)
+    Returns int value of the class.
+    """
+
+    class_labels = os.listdir(cut_dir_path)
     class_labels.sort()
 
     try:
         return_class = class_labels.index(folder_name)
     except ValueError:
-        print("Class was not found")
+        print("Class was not found. \
+            Function get_folder_class_index in data_helpers.")
         return_class = 0
     return return_class
